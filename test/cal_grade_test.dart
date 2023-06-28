e20f5ea644d54bcf0489753212aa836272d366f2
jirawat.pae <jirawat.pae@cpf.co.th>
2023-06-28 16:37:40 +0700

    add test case for grade B

--- test/cal_grade_test.dart
+++ test/cal_grade_test.dart
@@ -2,45 +2,52 @@ import 'package:cal_grade/cal_grade.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group("คำนวณ เกรด", () => {
-    test('1.ถ้า กรอก 90 คะแนน ได้เกรด A', () {
-        //arrange  สำหรับกำหนดค่าเริ่มต้นของการทดสอบ
-        String expectedValue = "A";
-        CalGrade calGrade = CalGrade();
+  group(
+      "คำนวณ เกรด",
+      () => {
+            test('1.ถ้า กรอก 90 คะแนน ได้เกรด A', () {
+              //arrange  สำหรับกำหนดค่าเริ่มต้นของการทดสอบ
+              String expectedValue = "A";
+              CalGrade calGrade = CalGrade();
 
-        //act สำหรับ execute ส่วนที่ต้องการทดสอบหรือ business logic
-        String actualValue = calGrade.grade(90);
+              //act สำหรับ execute ส่วนที่ต้องการทดสอบหรือ business logic
+              String actualValue = calGrade.grade(90);
 
+              //assert  สำหรับการตรวจสอบว่า ผลการทำงานตรงตามที่คาดหวังหรือไม่
+              expect(actualValue, expectedValue);
+            }),
+            test('2.ถ้า กรอก 65 คะแนน ได้เกรด C', () {
+              //arrange  สำหรับกำหนดค่าเริ่มต้นของการทดสอบ
+              String expectedValue = "C";
+              CalGrade calGrade = CalGrade();
 
-        //assert  สำหรับการตรวจสอบว่า ผลการทำงานตรงตามที่คาดหวังหรือไม่
-        expect(actualValue, expectedValue);
-      }),
+              //act สำหรับ execute ส่วนที่ต้องการทดสอบหรือ business logic
+              String actualValue = calGrade.grade(65);
 
-      test('2.ถ้า กรอก 65 คะแนน ได้เกรด C', () {
-        //arrange  สำหรับกำหนดค่าเริ่มต้นของการทดสอบ
-        String expectedValue = "C";
-        CalGrade calGrade = CalGrade();
+              //assert  สำหรับการตรวจสอบว่า ผลการทำงานตรงตามที่คาดหวังหรือไม่
+              expect(actualValue, expectedValue);
+            }),
+            test('3.ถ้า กรอก 49 คะแนน ได้เกรด F', () {
+              //arrange  สำหรับกำหนดค่าเริ่มต้นของการทดสอบ
+              String expectedValue = "F";
+              CalGrade calGrade = CalGrade();
 
-        //act สำหรับ execute ส่วนที่ต้องการทดสอบหรือ business logic
-        String actualValue = calGrade.grade(65);
+              //act สำหรับ execute ส่วนที่ต้องการทดสอบหรือ business logic
+              String actualValue = calGrade.grade(49);
 
+              //assert  สำหรับการตรวจสอบว่า ผลการทำงานตรงตามที่คาดหวังหรือไม่
+              expect(actualValue, expectedValue);
+            }),
+            test('4.ถ้า กรอก 70 คะแนน ได้เกรด B', () {
+              //arrange  สำหรับกำหนดค่าเริ่มต้นของการทดสอบ
+              String expectedValue = "B";
+              CalGrade calGrade = CalGrade();
 
-        //assert  สำหรับการตรวจสอบว่า ผลการทำงานตรงตามที่คาดหวังหรือไม่
-        expect(actualValue, expectedValue);
-      }),
+              //act สำหรับ execute ส่วนที่ต้องการทดสอบหรือ business logic
+              String actualValue = calGrade.grade(70);
 
-      test('3.ถ้า กรอก 49 คะแนน ได้เกรด F', () {
-        //arrange  สำหรับกำหนดค่าเริ่มต้นของการทดสอบ
-        String expectedValue = "F";
-        CalGrade calGrade = CalGrade();
-
-        //act สำหรับ execute ส่วนที่ต้องการทดสอบหรือ business logic
-        String actualValue = calGrade.grade(49);
-
-
-        //assert  สำหรับการตรวจสอบว่า ผลการทำงานตรงตามที่คาดหวังหรือไม่
-        expect(actualValue, expectedValue);
-      }),
-      
-  });
+              //assert  สำหรับการตรวจสอบว่า ผลการทำงานตรงตามที่คาดหวังหรือไม่
+              expect(actualValue, expectedValue);
+            }),
+          });
 }
